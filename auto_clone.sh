8c5e01680a18945a5ff184bd0e98e9d257736c5e
yanncousin1 <yanncousin1@penguin>
2022-11-09 16:45:20 +0100

    rm voyages.auto_clone.sh

---
+++ auto_clone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git rm voyages
+git add .
+git commit -m "rm voyages.auto_clone.sh"
+git push origin master
+
+git clone https://github.com/morgane-marechal/voyages
+
+rm -rf voyages/.git
+git rm --cache voyages
+
+git add .
+git commit -m "rm voyages cache.auto_clone.sh"
+git push origin master
+
+
+
