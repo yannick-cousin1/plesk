5159390cb811e7079b608797586eb2e58c97c8f9
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-11-10 09:12:26 +0100

    test automatic pull on plesk

--- auto_clone.sh
+++ auto_clone.sh
@@ -7,10 +7,7 @@ git add .
 git commit -m "removed voyages.auto_clone.sh"
 git push origin master
 
-git clone https://github.com/morgane-marechal/voyages
-
-rm -rf voyages/.git
-git rm --cache voyages
+git clone https://github.com/morgane-marechal/voyages && rm -rf voyages/.git && git rm --cache voyages
 
 git add .
 git commit -m "remove voyages cache.auto_clone.sh"
